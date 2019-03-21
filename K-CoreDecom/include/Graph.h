cabdb4c973af2cd20eebe6978c3c734297cb4362
ZHDou <1178647512@qq.com>
2019-03-21 16:54:14 +0800

    K-CoreDecomposition

---
+++ K-CoreDecom/include/Graph.h
@@ -0,0 +1,33 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+using namespace std;
+
+struct Edge{
+    int to,next;
+};
+
+
+class Graph
+{
+    public:
+        Graph();
+        virtual ~Graph();
+        void init(int n);
+        void add_Edge(int x,int y);
+        int get_N();
+        int get_M();
+        int get_Deg(int i);
+        bool isNeighbor(int x,int y);
+
+    protected:
+
+    private:
+        int n,m,tot;
+        vector<Edge>  edg;
+        vector<int> first;
+        vector<int> deg;
+};
+
+#endif // GRAPH_H
---
+++ K-CoreDecom/src/Graph.cpp
@@ -0,0 +1,61 @@
+#include "Graph.h"
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+Graph::Graph()
+{
+    //ctor
+}
+
+Graph::~Graph()
+{
+    //dtor
+}
+
+
+void Graph::init(int tn){
+    n = tn;
+    m = tot = 0;
+    for(int i=0; i<=n; i++) first.push_back(-1);
+    //printf("%d %d\n",tn,n);
+    for(int i=0; i<=n; i++) deg.push_back(0);
+}
+
+void Graph::add_Edge(int tx,int ty){
+    m++;
+    Edge e1;
+    e1.to = ty;
+    e1.next = first[tx];
+    edg.push_back(e1);
+    first[tx] = tot++;
+
+    e1.to = tx;
+    e1.next = first[ty];
+    edg.push_back(e1);
+    first[ty] = tot++;
+
+    deg[tx]++; deg[ty]++;
+}
+
+int Graph::get_N(){
+    return n;
+}
+
+int Graph::get_M(){
+    return m;
+}
+
+int Graph::get_Deg(int i){
+    return deg[i];
+}
+
+bool Graph::isNeighbor(int x, int y){
+    int k = first[x];
+    while(k != -1){
+        if(y == edg[k].to) return true;
+        k = edg[k].next;
+    }
+    return false;
+}
